05ab7e6d2a00d8fcf842bc7e9f5a85ea09d327fd
antenorr <roudyantenor@gmail.com>
2018-09-03 14:59:42 -0400

    last succesful view before revolutionary change to scripts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,14 @@ import React, { Component } from 'react';
 class Home extends Component {
     render () {
         return (
-            <h1>This will be our Welcome Home</h1>
+            <div className="homebody">
+                <h1>"The Tool maker"</h1>
+                <h3>Front-end Full-Stack Web Developer</h3>
+                <p>Based out of Miami Florida - i am the a full stack developer ready to work and get
+                    things done 
+                </p>
+            </div>
+            
         );
     }
 }
